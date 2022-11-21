9e43874e27b2ef1c07ea819436654199bb80d28b
Srdan21 <81490336+Srdan21@users.noreply.github.com>
2022-11-21 19:01:01 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ try {
 	console.log("Please create an auth.json like auth.json.example with a bot token or an email and password.\n"+e.stack); // send message for error - no token 
 }
 
-if(!AuthDetails.hasOwnProperty('bot_token')) {
+if(!AuthDetails.hasOwnProperty('MTA0MTQ3NjQxODQ2ODU4OTU5OQ.G7XFkM.3zcTDq_8CrjG4iq_NrcViavzT0IVTA79KkweSM')) {
 	//attempt to populate from ENV variables. useful for remote cloud deploys
 	AuthDetails = {
 		bot_token: process.env.bot_token,
